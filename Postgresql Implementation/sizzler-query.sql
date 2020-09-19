fd38ac85728dfca5ca66e4f71b23b79ea0d57f5d
ansntu <akangpanich94@gmail.com>
2020-09-19 13:56:50 +0700

    [Query] Rearrange No. of query in order

--- Postgresql Implementation/sizzler-query.sql
+++ Postgresql Implementation/sizzler-query.sql
@@ -1,5 +1,9 @@
 -- 6: Show date start and date end of all seasonal menu --
 SELECT * FROM "SeasonRef"
+-- 9: Show all of "western" serving menu --
+SELECT "servingRefId", "nameEng", "nameTha", "basePrice" FROM "ServingRef" WHERE genre = 'western'
+-- 12: Show the amount of table in each branch --
+SELECT TB."branchId", name, COUNT("tableId") FROM "Table" TB INNER JOIN "Branch" B on B."branchId" = TB."branchId"
 -- 15: Identify a menu that is the best seller in this month
 SELECT "nameEng",COUNT("OrderItem"."menuRefId") FROM "OrderItem" INNER JOIN "MenuRef" MR on MR."menuRefId" = "OrderItem"."menuRefId"
 GROUP BY "nameEng"
@@ -21,10 +25,6 @@ GROUP BY "memberCustomerId"
 -- 19: Identify rewards customer redeem it recently --
 SELECT concat("firstname",' ', "surname") AS "fullname", name FROM "MemberCustomer" INNER JOIN "MembershipRewardRedemption" MRR on "MemberCustomer"."memberCustomerId" = MRR."memberCustomerRefId" INNER JOIN "RedeemableRewardRef" RRR on RRR."redeemableRewardRefId" = MRR."redeemableRewardRefId"
 WHERE "memberCustomerId" = 'b7a57961-b4ae-46f3-bf63-e20960e9a16b' ORDER BY "timestamp" DESC LIMIT 1
--- 9: Show all of "western" serving menu --
-SELECT "servingRefId", "nameEng", "nameTha", "basePrice" FROM "ServingRef" WHERE genre = 'western'
--- 12: Show the amount of table in each branch --
-SELECT TB."branchId", name, COUNT("tableId") FROM "Table" TB INNER JOIN "Branch" B on B."branchId" = TB."branchId"
 GROUP BY TB."branchId", name
 -- 20: Identify member customer who order food within 7 days --
 SELECT "memberCustomerId", concat("firstname",' ', "surname") AS "fullname" FROM "MemberCustomer" INNER JOIN "Billing" B on "MemberCustomer"."memberCustomerId" = B."involvedMemberCustomerId"
