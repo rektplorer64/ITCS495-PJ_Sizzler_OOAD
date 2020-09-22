267dabf6a3b150ba6d007be189aa88fe246a4156
rektplorer64 <tanawin2009@hotmail.com>
2020-09-22 15:28:45 +0700

    Rerun the query number to categorize them into 2 groups: basic and advanced.

--- Postgresql Implementation/sizzler-query.sql
+++ Postgresql Implementation/sizzler-query.sql
@@ -1,4 +1,4 @@
--- 0.1: List all branches details in a given province of Thailand.
+-- A13 <0.1>: List all branches details in a given province of Thailand.
 CREATE OR REPLACE VIEW "BranchView" AS
 SELECT "B"."branchId",
        "name",
@@ -28,7 +28,7 @@ FROM "Branch" "B"
     GROUP BY "branchId"
 ) "EMP" ON "B"."branchId" = "EMP"."branchId";
 
--- 0.2: List the details of each menu with respects to current availability.
+-- A18 <0.2>: List the details of each menu with respects to current availability.
 SELECT "MR".*,
        "MenuAvailability"."menuRefId" IS NOT NULL "isAvailable",
        "MenuWitoutSeason"."menuRefId" IS NULL     "hasSeason",
@@ -64,7 +64,7 @@ FROM (SELECT * FROM "MenuRef" WHERE "isActive") "MR"
                     FROM "BranchMenuAvailability"
                     GROUP BY "menuRefId") "AvailableBranch" ON "AvailableBranch"."menuRefId" = "MR"."menuRefId";
 
--- 0.3: List all Inventory Inbound Order details grouped by branch.
+-- A15 <0.3>: List all Inventory Inbound Order details grouped by branch.
 SELECT "IIO"."inboundOrderId",
        "IIO"."branchId",
        "IIO"."timeCreated",
@@ -78,7 +78,7 @@ FROM "InventoryInboundOrder" "IIO"
          JOIN "FoodIngredientRef" "FIR" ON "FIR"."foodIngredientRefId" = "IIOI"."foodIngredientRefId"
          JOIN "QuantityUnitRef" "QUR" ON "IIOI"."quantityUnitRefId" = "QUR"."quantityUnitRefId";
 
--- 0.4: Given a menu, identify one or more servings that are included as well as the price of each serving and the percentage it made up to the menu price.
+-- A2 <0.4>: Given a menu, identify one or more servings that are included as well as the price of each serving and the percentage it made up to the menu price.
 SELECT "MR"."menuRefId",
        "MR"."nameEng"                                      "menuName",
        "MXR"."menuPrice",
@@ -96,27 +96,27 @@ FROM "MenuRef" "MR"
 ORDER BY "menuRefId";
 
 
--- 0.5: Identify the amount of each food item that needed to be refilled for the salad bar.
+-- A6 <0.5>: Identify the amount of each food item that needed to be refilled for the salad bar.
 SELECT "branchId", "SB"."saladBarId", "nameEng", "maxQuantity" || ' ' || "name" || 's' "maxQuantity", "descriptionTha"
 FROM "SaladBar" "SB"
          JOIN "SaladBarServing" "SBS" ON "SB"."saladBarId" = "SBS"."saladBarId"
          JOIN "FoodItemRef" "FIR" ON "SBS"."foodItemRefId" = "FIR"."foodItemRefId"
          JOIN "QuantityUnitRef" "QUR" ON "SBS"."maxQuantityUnit" = "QUR"."quantityUnitRefId";
 
--- 1: Identify the cash transaction that has the highest amount.
+-- A7 <1>: Identify the cash transaction that has the highest amount.
 SELECT "PT".*, "CT"."amount"
 FROM "PaymentTransaction" "PT"
          JOIN "CashTransaction" "CT" ON "PT"."paymentTransactionId" = "CT"."paymentTransactionId"
 ORDER BY "amount" DESC
 LIMIT 1;
 
--- 2: List food items in salad bar that are needed to be refilled.
+-- B7 <2>: List food items in salad bar that are needed to be refilled.
 SELECT "SBR"."foodItemRefId", "FIR"."nameTha" "name", ("SBR"."quantity" || ' ' || "QUR"."name") "amount"
 FROM "FoodItemRef" "FIR"
          JOIN "SaladBarRefill" "SBR" ON "FIR"."foodItemRefId" = "SBR"."foodItemRefId"
          JOIN "QuantityUnitRef" "QUR" ON "SBR"."quantityUnit" = "QUR"."quantityUnitRefId";
 
--- 3: List all customers with the level of membership, point gained, as well as the amount of money he/she spent.
+-- A14 <3>: List all customers with the level of membership, point gained, as well as the amount of money he/she spent.
 CREATE OR REPLACE VIEW "MemberCustomerView" AS
 SELECT "A"."memberCustomerId",
        "firstname",
@@ -163,13 +163,13 @@ FROM (
 ) "B" ON "A"."billingId" = "B"."billingId"
 GROUP BY "A"."memberCustomerId", "firstname", "surname", "telephoneNo", "email", "membershipLevel";
 
--- 4: List the worth of each gift voucher in each gift voucher transaction.
+-- B12 <4>: List the worth of each gift voucher in each gift voucher transaction.
 SELECT "GVT"."paymentTransactionId", "GVT"."giftVoucherNo", "name", "valueAmount"
 FROM "GiftVoucherTransaction" "GVT"
          JOIN "GiftVoucher" "GV" ON "GVT"."giftVoucherNo" = "GV"."giftVoucherNo"
          JOIN "GiftVoucherRef" "GVR" ON "GV"."giftVoucherRefId" = "GVR"."giftVoucherRefId";
 
--- 5: Summarize each employee's the total amount of working time as well as the wage payments.
+-- A24 <5>: Summarize each employee's the total amount of working time as well as the wage payments.
 SELECT "A"."employeeId",
        "firstname",
        "surname",
@@ -204,7 +204,7 @@ GROUP BY "A"."employeeId", "firstname", "surname", "nickname", "email", "phoneNu
          "birthdate",
          "age";
 
--- 6: Show the availability time range of all seasonal menu.
+-- B16 <6>: Show the availability time range of all seasonal menu.
 SELECT "MR"."menuRefId",
        "MR"."nameEng",
        "SR"."name" "seasonName",
@@ -214,7 +214,7 @@ FROM "SeasonRef" "SR"
          JOIN "MenuSeasonRef" "MSR" ON "SR"."seasonRefId" = "MSR"."seasonRefId"
          JOIN "MenuRef" "MR" ON "MR"."menuRefId" = "MSR"."menuRefId";
 
--- 7: Identify the common availability of each menu. For example, a menu can only specifically available from anytime except 3 PM to 10 PM on Wednesday to Sunday.
+-- B3 <7>: Identify the common availability of each menu. For example, a menu can only specifically available from anytime except 3 PM to 10 PM on Wednesday to Sunday.
 SELECT "MR"."menuRefId",
        "MR"."nameTha",
        "MA"."dayOfWeek",
@@ -223,24 +223,24 @@ SELECT "MR"."menuRefId",
 FROM "MenuAvailability" "MA"
          JOIN "MenuRef" "MR" ON "MA"."menuRefId" = "MR"."menuRefId";
 
--- 8: Identify the duration it takes to complete each inventory supply inbound order.
+-- B4 <8>: Identify the duration it takes to complete each inventory supply inbound order.
 SELECT *
 FROM "InventoryInboundOrder" "IIO"
          JOIN "InventoryInboundOrderItem" "IIOI" ON "IIO"."inboundOrderId" = "IIOI"."inboundOrderId";
 
--- 9: List the details of all "western" food servings.
+-- B9 <9>: List the details of all "western" food servings.
 SELECT "servingRefId", "nameEng", "nameTha", "basePrice"
 FROM "ServingRef"
 WHERE "genre" = 'western';
 
--- 10: List the details of food items given a serving.
+-- B10 <10>: List the details of food items given a serving.
 SELECT "ServingFoodItemRef"."servingRefId", "ServingRef"."nameEng", "FoodItemRef"."nameEng"
 FROM "ServingRef"
          JOIN "ServingFoodItemRef" ON "ServingRef"."servingRefId" = "ServingFoodItemRef"."servingRefId"
          JOIN "FoodItemRef" ON "FoodItemRef"."foodItemRefId" = "ServingFoodItemRef"."foodItemRefId"
 ORDER BY "servingRefId";
 
--- 11: List the delivery time, distance and average speed take to finish each delivery.
+-- A25 <11>: List the delivery time, distance and average speed take to finish each delivery.
 SELECT "DM".*,
        "BD"."timeUsed",
        "BD"."distanceKM",
@@ -255,23 +255,23 @@ FROM (
          JOIN "BillingDelivery" "BD" ON "DM"."deliveryManId" = "BD"."deliveryManId"
          JOIN "CustomerDelivery" "CD" ON "BD"."billingId" = "CD"."deliveryBillingId";
 
--- 12: Identify the number of available tables in each branch.
+-- A8 <12>: Identify the number of available tables in each branch.
 SELECT "TB"."branchId", "name", COUNT("tableId") AS "tableAmount"
 FROM "Table" "TB"
          INNER JOIN "Branch" "B" ON "B"."branchId" = "TB"."branchId"
 GROUP BY "TB"."branchId", "name";
 
--- 13: List all gift voucher copies that are already used.
+-- B6 <13>: List all gift voucher copies that are already used.
 SELECT "giftVoucherNo"
 FROM "GiftVoucherTransaction";
 
--- 14: Show the number of customers within a day.
+-- A22 <14>: Show the number of customers within a day.
 SELECT "timeAdded"::DATE, SUM("totalCustomers") AS "TotalInDay"
 FROM "CustomerPax"
 WHERE "timeAdded" BETWEEN '2020-02-08 00:00:00' AND '2020-02-08 23:59:59'
 GROUP BY "timeAdded"::DATE;
 
--- 15: Identify the top-3 best seller menu in this month.
+-- A10 <15>: Identify the top-3 best seller menu in this month.
 SELECT "nameEng", COUNT("OrderItem"."menuRefId") AS "saleAmount"
 FROM "OrderItem"
          INNER JOIN "MenuRef" "MR" ON "MR"."menuRefId" = "OrderItem"."menuRefId"
@@ -280,7 +280,7 @@ GROUP BY "nameEng"
 ORDER BY "saleAmount" DESC
 LIMIT 3;
 
--- 16: Identify the top-3 member customers who spend the most.
+-- A11 <16>: Identify the top-3 member customers who spend the most.
 SELECT "memberCustomerId", concat("firstname", ' ', "surname") AS "fullname", SUM("price") AS "overallPrice"
 FROM "MemberCustomer"
          INNER JOIN "Billing" "B" ON "MemberCustomer"."memberCustomerId" = "B"."involvedMemberCustomerId"
@@ -290,7 +290,7 @@ GROUP BY "memberCustomerId", "fullname"
 ORDER BY "overallPrice" DESC
 LIMIT 3;
 
--- 17: Show the assigned work time of every employee.
+-- B15 <17>: Show the assigned work time of every employee.
 SELECT "EV"."employeeId",
        "firstname",
        "surname",
@@ -301,12 +301,12 @@ FROM "EmployeeView" "EV"
          JOIN "WorkTime" "WT" ON "EV"."employeeId" = "WT"."employeeId"
 GROUP BY "EV"."employeeId", "firstname", "surname", "age", "workAtBranch";
 
--- 18: Identify MAC Addresses of every computer in a branch.
+-- B2 <18>: Identify MAC Addresses of every computer in a branch.
 SELECT "macAddress", "Branch"."name"
 FROM "ComputerMachine"
          JOIN "Branch" ON "Branch"."branchId" = "ComputerMachine"."branchId";
 
--- 19: Identify redeemable rewards that a customer recently redeem.
+-- A4 <19>: Identify redeemable rewards that a customer recently redeem.
 SELECT concat("firstname", ' ', "surname") AS "fullname", "name"
 FROM "MemberCustomer"
          INNER JOIN "MembershipRewardRedemption" "MRR"
@@ -316,13 +316,13 @@ WHERE "memberCustomerId" = 'b7a57961-b4ae-46f3-bf63-e20960e9a16b'
 ORDER BY "timestamp" DESC
 LIMIT 1;
 
--- 20: Identify member customers who have ordered foods within 7 days.
+-- A3 <20>: Identify member customers who have ordered foods within 7 days.
 SELECT "memberCustomerId", concat("firstname", ' ', "surname") AS "fullname"
 FROM "MemberCustomer"
          INNER JOIN "Billing" "B" ON "MemberCustomer"."memberCustomerId" = "B"."involvedMemberCustomerId"
 WHERE "B"."timePaid" > now() - INTERVAL '1 week';
 
--- 21: List the details of orders given a billing information.
+-- A19 <21>: List the details of orders given a billing information.
 SELECT "Order"."orderId", "nameEng", "nameTha", "realPrice", "Order"."timeCreated"
 FROM "Order"
          INNER JOIN "Billing" "B" ON "B"."billingId" = "Order"."billingId"
@@ -330,7 +330,7 @@ FROM "Order"
          INNER JOIN "MenuRef" "MR" ON "MR"."menuRefId" = "OI"."menuRefId"
          INNER JOIN "MenuServingRef" "MSR" ON "MR"."menuRefId" = "MSR"."menuRefId";
 
--- 22: Show food ingredients in each food item.
+-- A21 <22>: Show food ingredients in each food item.
 SELECT "FoodItemRef"."nameTha", "FoodIngredientRef"."nameTha", "quantity", "QuantityUnitRef"."name"
 FROM "FoodItemRef"
          JOIN "FoodItemIngredientRef" ON "FoodItemRef"."foodItemRefId" = "FoodItemIngredientRef"."foodItemRefId"
@@ -338,46 +338,46 @@ FROM "FoodItemRef"
               ON "FoodItemIngredientRef"."foodIngredientRef" = "FoodIngredientRef"."foodIngredientRefId"
          JOIN "QuantityUnitRef" ON "FoodItemIngredientRef"."quantityUnitRefId" = "QuantityUnitRef"."quantityUnitRefId";
 
--- 23: Show the details of menu in each order.
+-- B17 <23>: Show the details of menu in each order.
 SELECT "OrderItem"."orderId", "MenuRef"."nameTha", "MenuRef"."descriptionTha", "OrderItem"."perUnitPrice"
 FROM "MenuRef"
          JOIN "OrderItem" ON "MenuRef"."menuRefId" = "OrderItem"."menuRefId";
 
--- 24: List the details of seasonal menus.
+-- B11 <24>: List the details of seasonal menus.
 SELECT "SeasonRef"."name", "nameTha", "descriptionTha"
 FROM "MenuRef"
          JOIN "MenuSeasonRef" ON "MenuRef"."menuRefId" = "MenuSeasonRef"."menuRefId"
          JOIN "SeasonRef" ON "MenuSeasonRef"."seasonRefId" = "SeasonRef"."seasonRefId";
 
--- 25: Show employees in each branch.
+-- B15 <25>: Show employees in each branch.
 SELECT "firstname", "surname", "name"
 FROM "Employee"
          JOIN "Branch" "B" ON "B"."branchId" = "Employee"."branchId";
 
--- 26: Show the total distance in kilometers that a delivery man has ever delivered.
+-- A23 <26>: Show the total distance in kilometers that a delivery man has ever delivered.
 SELECT "BillingDelivery"."deliveryManId", SUM("distanceKM") AS "TotalDistance"
 FROM "BillingDelivery"
          JOIN "DeliveryMan" ON "BillingDelivery"."deliveryManId" = "DeliveryMan"."employeeId"
 WHERE "BillingDelivery"."deliveryManId" = "DeliveryMan"."employeeId"
 GROUP BY "BillingDelivery"."deliveryManId";
 
--- 27: Show the salary of all Kitchen Managers
+-- B18 <27>: Show the salary of all Kitchen Managers
 SELECT "wagePaymentAmount" * 30 AS "salary", "wageBonusAmount", "wagePaymentAmount" * 30 + "wageBonusAmount" AS "total"
 FROM "EmployeeWagePayment"
          JOIN "KitchenManager" ON "EmployeeWagePayment"."employeeId" = "KitchenManager"."employeeId";
 
--- 28: Show all employees' address that are located in Chonburi
+-- B13 <28>: Show all employees' address that are located in Chonburi
 SELECT "E".*
 FROM "Employee" "E"
          JOIN "Province" ON "E"."provinceId" = "Province"."provinceId"
 WHERE "nameEnglish" = 'Chonburi Province';
 
--- 29: List all employees' full name that has the age greater than or equal 40 years old.
+-- B5 <29>: List all employees' full name that has the age greater than or equal 40 years old.
 SELECT "employeeId", CONCAT("firstname", ' ', "surname") AS "fullName", "age"
 FROM "Employee"
 WHERE "age" >= 40;
 
--- 30: List the details of each billing in detail in terms of its type, timestamp, responsible employee, and other related information.
+-- A17 <30>: List the details of each billing in detail in terms of its type, timestamp, responsible employee, and other related information.
 CREATE OR REPLACE VIEW "BillingView" AS
 SELECT "00".*,
        CASE
@@ -420,7 +420,7 @@ FROM (
              JOIN "EmployeeView" "EV" ON "CBH"."cashierId" = "employeeId"
 ) "BB" ON "00"."billingId" = "BB"."billingId";
 
--- 31: Identify the top-10 credit transactions that have the highest amount.
+-- A9 <31>: Identify the top-10 credit transactions that have the highest amount.
 SELECT "CT"."paymentTransactionId", "amount", "taxInvoiceId", "timeCreated", "status", "pointReceived"
 FROM "CreditTransaction" "CT"
          JOIN "PaymentTransaction" "PT" ON "CT"."paymentTransactionId" = "PT"."paymentTransactionId"
@@ -428,7 +428,7 @@ FROM "CreditTransaction" "CT"
 ORDER BY "amount" DESC
 LIMIT 10;
 
--- 32: List all payment transactions that are ever made by any customer in any billing.
+-- A16 <32>: List all payment transactions that are ever made by any customer in any billing.
 CREATE OR REPLACE VIEW "PaymentTransactionView" AS
 SELECT "PT"."paymentTransactionId",
        "timestamp",
@@ -448,20 +448,20 @@ FROM "PaymentTransaction" "PT"
          LEFT JOIN "CreditTransaction" "CT2" ON "PT"."paymentTransactionId" = "CT2"."paymentTransactionId"
          LEFT JOIN "GiftVoucherTransaction" "GVT" ON "PT"."paymentTransactionId" = "GVT"."paymentTransactionId";
 
--- 33: Count employees group by age
+-- B1 <33>: Count employees group by age
 SELECT "age", COUNT("employeeId") AS "numberOfEmployees"
 FROM "Employee"
 GROUP BY "age"
 ORDER BY "age";
 
--- 34: Identify the age that has the highest number of employees
+-- A5 <34>: Identify the age that has the highest number of employees
 SELECT "age", COUNT("employeeId") "numberOfEmployees"
 FROM "Employee"
 GROUP BY "age"
 ORDER BY COUNT("employeeId") DESC
 LIMIT 1;
 
--- 35: Count all member customers that are grouped by each membership level.
+-- A1 <35>: Count all member customers that are grouped by each membership level.
 SELECT "MLR"."name", COUNT("memberCustomerId") "numberOfMembers"
 FROM "MemberCustomer" "MC"
          JOIN "MembershipRewardRedemption" "MRR"
@@ -471,11 +471,11 @@ FROM "MemberCustomer" "MC"
          JOIN "MemberLevelRef" "MLR" ON "MLRO"."memberLevelRefId" = "MLR"."memberLevelRefId"
 GROUP BY "MLR"."name";
 
--- 36: Show customer first name, surname, and member level.
+-- A20 <36>: Show customer first name, surname, and member level.
 SELECT "MCV"."memberCustomerId", "MCV"."firstname", "MCV"."surname", "MCV"."membershipLevel"
 FROM "MemberCustomerView" "MCV";
 
--- 37: Identify the total sale frequency of each menu.
+-- A12 <37>: Identify the total sale frequency of each menu.
 SELECT "MR"."menuRefId", "nameEng", COUNT("quantity") "sellCount"
 FROM "MenuRef" "MR"
          JOIN "OrderItem" "OI" ON "MR"."menuRefId" = "OI"."menuRefId"
